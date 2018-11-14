e62389aee502e03aee0719f11afa56f644e1d059
Teja Pristavec <tp2sk@virginia.edu>
2018-11-14 09:41:56 -0500

    Merged data frames and wrote out city-level data.

--- src/scrape_cities.R
+++ src/scrape_cities.R
@@ -1,9 +1,11 @@
 library(rvest)
 library(dplyr)
 library(stringr)
+library(readr)
 
 
 # Daniel helped me with the "assign" part. Thank you Daniel :)
+# File will write out to /src/us_cities.csv (assuming Rproj).
 
 
 #
@@ -18,15 +20,12 @@ states <- states[[1]]
 
 # Get state names only
 state_names <- states$`State Name`
-state_names
 
 # Fix blank space to - for URLs
 state_names <- str_replace_all(state_names, " ", "-")
-state_names 
 
 # Make state URLs
 state_list <- str_c("https://broadbandnow.com/", state_names)
-state_list
 
 
 #
@@ -43,19 +42,26 @@ for (val_i in 1:length(state_list)) {
     mutate(State = state_names[val_i]) %>%
     select(-`Average speed over time`)
   
-  assign(x = str_to_lower(state_names[val_i]), data)
+  assign(paste("frame", str_to_lower(state_names[val_i]), sep = "_"), data)
 }
 
 
 #
-#------------------------------------ Merge and write out
-#
+#------------------------------------ Merge and write out 
 
-# Working on this Wednesday
+
+# Make a list of dataframes
+dflist <- lapply(ls(pattern = "frame_"), function(x) if (class(get(x)) == "data.frame") get(x))
+
+# Bind data frames
+us_cities <- do.call("rbind", dflist)
+
+# Write out
+write_csv(us_cities, path = "/src/us_cities.csv")
 
 
 #
 #------------------------------------ Clean up workspace
 #
 
-remove(list = c(ls(pattern = "cities"), ls(pattern = "data"), ls(pattern = "state"), ls(pattern = "val_i")))
+remove(list = ls())
