97ddfebb0be53f6dbf892008e0f5bb58a4b72fc5
Teja Pristavec <tp2sk@virginia.edu>
2018-11-19 17:12:34 +0000

    Cleaned up code and added ggplot

--- src/rus_broadband_ineligibility.R
+++ src/rus_broadband_ineligibility.R
@@ -1,24 +1,14 @@
 library(sf)
 library(dplyr)
-
-
-# Note: Data files from http://www2.census.gov/geo/tiger/GENZ2016/shp/ (cb_2016_us_state_20m) 
-# and https://www.sc.egov.usda.gov/data/data_files.html (RD_BROADBAND_IELG_PRODUCTION). 
-# Not on GitLab. Change to local paths.
-
+library(ggplot2)
 
 #
 #------------------------------------------- Read in data
 #
 
-#ineligibility <- st_read("~/Broadband_Ineligible/RD_BROADBAND_IELG_PRODUCTION.shp", 
-#                         stringsAsFactors = FALSE)
 
 ineligibility <- st_read("data/rus_broadband_ineligibility/RD_BROADBAND_IELG_PRODUCTION.shp", 
                          stringsAsFactors = FALSE)
-
-#us_states <- st_read("~/cb_2016_us_state_20m/cb_2016_us_state_20m.shp", 
-#                     stringsAsFactors = FALSE)
 
 us_states <- st_read("data/census_geo_cb/cb_2016_us_state_20m.shp", 
                      stringsAsFactors = FALSE)
@@ -44,15 +34,15 @@ ineligibility <- st_transform(ineligibility, project)
 # Virignia
 va <- us_states[us_states$NAME == "Virginia", ]
 va <- va %>% select(geometry)
+
 intersection <- st_intersection(va, ineligibility)
 
+# Base R
 plot(va, reset = FALSE)
 plot(intersection, add = TRUE)
 
-#plot(st_intersection(va, ineligibility), add=T)
-
-# Georgia
-ga <- us_states[us_states$NAME == "Georgia", ]
-ga <- ga %>% select(geometry)
-
-plot(st_intersection(ga, ineligibility))
+# Ggplot
+ggplot() +
+  geom_sf(data = va) +
+  geom_sf(data = st_intersection(va, ineligibility), fill = "orange") + 
+  labs(title = "Map of RUS program ineligibility in VA")
