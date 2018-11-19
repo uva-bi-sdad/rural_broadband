4e11fd5e726620ad349edf833a6ed2f550f6d59a
Teja Pristavec <tp2sk@virginia.edu>
2018-11-19 10:55:45 -0500

    Added ineligibility mapping code

--- src/rus_broadband_ineligibility.R
+++ src/rus_broadband_ineligibility.R
@@ -1,16 +1,48 @@
 library(sf)
+library(dplyr)
 
-sp <- st_read("data/rus_broadband_ineligibility/RD_BROADBAND_IELG_PRODUCTION.shp", stringsAsFactors = FALSE)
 
-plot(sp)
+# Note: Data files from http://www2.census.gov/geo/tiger/GENZ2016/shp/ (cb_2016_us_state_20m) 
+# and https://www.sc.egov.usda.gov/data/data_files.html (RD_BROADBAND_IELG_PRODUCTION). 
+# Not on GitLab. Change to local paths.
 
-us_states <- st_read("data/census_geo_cb/cb_2016_us_state_20m.shp", stringsAsFactors = FALSE)
-va <- us_states[us_states$STATEFP=="51",]
-plot(va)
 
-d <- st_crs(va)
-sp2 <- st_transform(sp, d)
+#
+#------------------------------------------- Read in data
+#
 
-jn <- st_join(sp2, va, join = st_intersects)
+ineligibility <- st_read("~/Broadband_Ineligible/RD_BROADBAND_IELG_PRODUCTION.shp", 
+                         stringsAsFactors = FALSE)
 
-plot(jn)
+us_states <- st_read("~/cb_2016_us_state_20m/cb_2016_us_state_20m.shp", 
+                     stringsAsFactors = FALSE)
+
+
+#
+#------------------------------------------- Match projections
+#
+
+project <- st_crs(us_states)
+ineligibility <- st_transform(ineligibility, project)
+
+
+#
+#------------------------------------------- Map ineligibility by state
+#
+
+# Notes: This runs and *looks* like it makes sense (cf. ilegibility map and state maps separately), but I still get the following warning: 
+# although coordinates are longitude/latitude, st_intersection assumes that they are planar
+# Warning message:
+#  attribute variables are assumed to be spatially constant throughout all geometries 
+
+# Virignia
+va <- us_states[us_states$NAME == "Virginia", ]
+va <- va %>% select(geometry)
+
+plot(st_intersection(va, ineligibility))
+
+# Georgia
+ga <- us_states[us_states$NAME == "Georgia", ]
+ga <- ga %>% select(geometry)
+
+plot(st_intersection(ga, ineligibility))
