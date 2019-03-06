04110a5184c306b29fae5bc961bb50a84cecb295
Teja Pristavec <tp2sk@virginia.edu>
2019-03-06 19:46:06 +0000

    Added inverse gaussian

--- src/property_glm.R
+++ src/property_glm.R
@@ -2,6 +2,17 @@ library(dplyr)
 library(naniar)
 library(stargazer)
 
+# GLM
+# No assumption of normality, constant variance, or additivity of effects. Instead:
+# response variable is assumed to be a member of the exponential family of distributions; 
+# variance is permitted to vary with the mean of the distribution; 
+# the effect of covariates on the response variable is assumed to be additive on a transformed scale. 
+
+# Resources:
+# https://www.towerswatson.com/en/Insights/IC-Types/Technical-Regulatory/2010/A-Practitioners-Guide-to-Generalized-Linear-Models
+
+# Link functions: GLMs require that there is a link function that guarantees additivity. LM requires that Y is additive in covariates; 
+# GLM requires that there is some transformation of Y, g(Y), that is additive in the covariates.
 
 #
 # Read in & prepare data --------------------------------------------------------------------------------------------------------------
@@ -24,12 +35,15 @@ nomiss <- propdata %>% select(GEOID, STATEFP, COUNTYFP, NAME, name2, available, 
 # GLM gamma: The outcome is continuous, right skewed, and always positive. -------------------------------------------------------------
 #
 
+# "... gamma distribution more strongly influenced by points on the left because the model assumes the variance increases with the square of the expected value"
+
 # subscription_continuous excluded (available included)
 
 # Identity, log, and inverse links are possible.
 # Inverse and identity do not converge.
 
 # Log link: Multiplicative arithmetic mean model
+# Log link: effects of covariates are multiplicative; GLM estimates logs of multiplicative effects.
 # Interpretation: The exponentiated coefficient of X represents the arithmetic mean ratio in the dependent variable between the X01 and X=0, aka
 # the exponentiated coefficient is the multiplier on the expected value of Y when X changes by 1.
 
@@ -64,6 +78,27 @@ autoplot(fit_gamma3)
 
 # The outlier is the same (22547) as it was in the OLS.
 
-# GLM inverse gaussian
+
+#
+# GLM inverse gaussian -------------------------------------------------------------
+#
+
+# subscription_continuous excluded (available included)
+
 # Identity, log, inverse, and 1/mu^2 links are possible.
+# Identity, inverse, 1/mu^2 do not converge.
+
+# Log link
+# Untransformed estimates
+fit_invg1 <- glm(median_val ~ available,
+                  data = nomiss, family = inverse.gaussian(link = "log"))
+fit_invg2 <- glm(median_val ~ available +
+                    hs_or_less + age_65_older + hispanic + black + foreign + rural + poverty + density + family,
+                  data = nomiss, family = inverse.gaussian(link = "log"))
+fit_invg3 <- glm(median_val ~ available +
+                    hs_or_less + age_65_older + hispanic + black + foreign + rural + poverty + density + family +
+                    rate_occupied + rate_owned + rate_single + median_yrbuilt,
+                  data = nomiss, family = inverse.gaussian(link = "log"))
+
+stargazer(fit_invg1, fit_invg2, fit_invg3, digits = 2, no.space = TRUE, type = "text", align = TRUE, df = TRUE, star.cutoffs = c(0.05, 0.01, 0.001))
 
