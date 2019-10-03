f1eaa362855ae28f8f0a653d93a85bdbdf3844d1
Devika Nair <dnair@virginia.edu>
2019-10-03 18:27:14 +0000

    freaking out

--- src/bip/newpenderfile/penderbipvariable.R
+++ src/bip/newpenderfile/penderbipvariable.R
@@ -17,3 +17,23 @@ bip_approved_join_comb <- st_union(bip_approved_join)
 tryme$bip <- st_within(tryme, bip_approved_join_comb) %>% lengths > 0
 tryme %>% filter(bip == TRUE)
 
+#####
+
+deedtaxmergeall <- readRDS("~/git/rural_broadband/data/Merged_Data_Smaller_States-John_Pender_18-09-2019/deed_tax_merge_all.RDS")
+write.csv(deedtaxmergeall, "~/git/rural_broadband/data/Merged_Data_Smaller_States-John_Pender_18-09-2019/deed_tax_merge_all.csv", sep = "|")
+
+
+deedtaxmergeall <- deedtaxmergeall %>% select(apnparcelnumberunformatted, state, propertylevellatitude, propertylevellongitude)
+
+deedtaxmergeall <- st_as_sf(deedtaxmergeall, coords = c("propertylevellatitude", "propertylevellongitude")) 
+
+bip_approved_join <- readRDS("~/git/rural_broadband/data/working/BIP_working/BIP_Approved.RDS") 
+
+bip_approved_join_comb <- st_union(bip_approved_join)
+st_crs(deedtaxmergeall) <- st_crs(bip_approved_join)
+deedtaxmergeall$bip <- st_within(deedtaxmergeall, bip_approved_join_comb) %>% lengths > 0
+deedtaxmergeall %>% filter(bip == TRUE)
+
+
+
+
